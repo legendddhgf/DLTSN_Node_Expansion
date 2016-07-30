e9b35cc0c6c5ff4f936227ac7b0aa9fc174c2c1f
legendddhgf <legendddhgf@gmail.com>
2016-07-29 17:50:25 -0700

    IterationClient works for arduinos and mate3's at the same time
    Functionality made to determine whether something is designed for the mate or arduino
    and publishes happen accordingly
    
    Fixed bug where due to asynchronous operation, an item can no longer be in the
    dictionary during the iteration thread (at loop start it is but during the loop,
    the item is deleted)

--- iterationClient.py
+++ iterationClient.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 # Dual threaded implementation of an iteration client:
 # one thread will update the nodeList, while the other one iterates through it
 # Takes in a command as such:
@@ -14,53 +16,86 @@ import paho.mqtt.publish as publish
 import threading
 import time
 import binascii
+import argparse
 
 nodeList = {}
 BROKER_NAME = "127.0.0.1"
 # BROKER_NAME = "128.114.63.86"
 
 def on_message(mqttc, obj, msg):
-	global nodeList
+    global nodeList
+    
+    device = "Atmega2560"
+    tempstr = msg.topic.split("/")[2]
+    if tempstr == 'mate3':
+        device = "Mate3"
+    if device == "Atmega2560":
+        mac = msg.topic.split("/")[2]
+        condition = msg.payload.split(" ")[0]   
+        argument = msg.payload.split(" ")[1]
 
-	mac = msg.topic.split("/")[2]
-	condition = msg.payload.split(" ")[0]	
-	argument = msg.payload.split(" ")[1]
+        # If payload is not a properly formed command in hex,
+        # convert to hex and make sure the result is terminated with a carriage return. 
+        if "\x0A" not in argument:
+            conv = argument.encode("hex") + "0A"
+            argument = binascii.unhexlify(conv)
 
-    # If payload is not a properly formed command in hex,
-    # convert to hex and make sure the result is terminated with a carriage return. 
-	if "\x0A" not in argument:
-		conv = argument.encode("hex") + "0A"
-		argument = binascii.unhexlify(conv)
+        print "received mac address: " + mac
+        if condition == 'START':
+            print "starting: " + argument
+    
+            nodeList[mac + ',' + argument] = argument
+        else:
+            print "stopping:" + argument
+            if (mac + ',' + argument) in nodeList:
+                if nodeList[mac + ',' + argument] == argument:
+                    del nodeList[mac + ',' + argument]
+    elif device == "Mate3":
+        SerNo = msg.topic.split("/")[3]
+        condition = msg.payload.split(" ")[0]
+        argument = msg.payload.split(" ")[1]
 
-	print "received mac address: " + mac
-	if condition == 'START':
-		print "starting: " + argument
+        # If payload is not a properly formed command in hex,
+        # convert to hex and make sure the result is terminated with a carriage return. 
+        if "\x0A" not in argument:
+            conv = argument.encode("hex") + "0A"
+            argument = binascii.unhexlify(conv)
 
-		nodeList[mac + ',' + argument] = argument
-	else:
-		print "stopping:" + argument
-		if (mac + ',' + argument) in nodeList:
-			if nodeList[mac + ',' + argument] == argument:
-				del nodeList[mac + ',' + argument]
-	
+        print "received serial number: " + SerNo
+        if condition == 'START':
+            print "starting mate command: " + argument
+            nodeList['mate3,' + SerNo + ',' + argument] = argument
+        else:
+            print "stopping: mate command: " + argument
+            if ('mate3,' + SerNo + ',' + argument) in nodeList:
+                del nodeList['mate3,' + SerNo + ',' + argument]
+    else:
+        print "Invalid device error in iteration client"
+        exit()
 
 def mqttThread():
-	client = mqtt.Client("iterationClient")
-	client.on_message = on_message
-	client.connect(BROKER_NAME, 1883)
-	client.subscribe("testbed/iterationClient/#", 0)
-	client.loop_forever()
+    client = mqtt.Client("iterationClient")
+    client.on_message = on_message
+    client.connect(BROKER_NAME, 1883)
+    client.subscribe("testbed/iterationClient/#", 0)
+    client.loop_forever()
 
 def iterationThread():
-	print "in publishing"
-	while(True):
-		iterList = nodeList
-		# print iterList
-		# For each mac address, publish to that mac address
-		for i in iterList.keys():
-			print "publishing: " + iterList[i] + " MAC ADDRESS: " + i.split(',')[0]
-			publish.single("testbed/gateway/mqtt/" + i.split(',')[0], iterList[i], hostname=BROKER_NAME)
-			time.sleep(1)
+    print "in publishing"
+    while(True):
+        iterList = nodeList
+        # print iterList
+        # For each mac address, publish to that mac address
+        for i in iterList.keys():
+            if i not in iterList:
+                break
+            if i.split(',')[0] == 'mate3':
+                print "publishing mate3 command: " + iterList[i] + " SERIAL NUMBER: " + i.split(',')[1]
+                publish.single("testbed/gateway/mqtt/mate3/" + i.split(',')[1], iterList[i], hostname=BROKER_NAME)
+            else:
+                print "publishing: " + iterList[i] + " MAC ADDRESS: " + i.split(',')[0]
+                publish.single("testbed/gateway/mqtt/" + i.split(',')[0], iterList[i], hostname=BROKER_NAME)
+            time.sleep(1)
 
 t1 = threading.Thread(target=mqttThread)
 t2 = threading.Thread(target=iterationThread)
